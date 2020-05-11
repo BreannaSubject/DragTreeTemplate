c9eb4669b3c3163f06da7a481f52bca87ca2a73b
Breanna Subject <subjectkids@gmail.com>
2020-05-11 13:17:07 -0400

    Added required code

--- DragTree/Form1.cs
+++ DragTree/Form1.cs
@@ -13,50 +13,89 @@ namespace DragTree
 {
     public partial class Form1 : Form
     {
-        // TODO - create an int variable to track currentRow,
-        // TODO - create a Stopwatch object called stopwatch 
-        // TODO - create a timer on the form called lightTimer (interval 400ms)
+        #region global variables
+        int currentRow;
+        Stopwatch myWatch = new Stopwatch();
+        
 
+        #endregion
         public Form1()
         {
             InitializeComponent();
         }
 
+        #region startButton
         private void startButton_Click(object sender, EventArgs e)
         {
-            // TODO - start the timer
+            lightTimer.Enabled = true;
+           
 
         }
 
         private void lightTimer_Tick(object sender, EventArgs e)
         {
-            // TODO - attacht this method to the lightTimer that was added on the form
-            // TODO - create a switch block that checks currentRow. In each case
-            // it will light up the appropriate lights, (labels). 
+            currentRow++;
 
-            // TODO - increment the currentRow value by 1
+            switch (currentRow)
+            {
+                case 1:
+                    row1col1.BackColor = Color.Goldenrod;
+                    row1col2.BackColor = Color.Goldenrod;
+                    break;
+                case 2:
+                    row2col1.BackColor = Color.Goldenrod;
+                    row2col2.BackColor = Color.Goldenrod;
+                    break;
+                case 3:
+                    row3col1.BackColor = Color.Goldenrod;
+                    row3col2.BackColor = Color.Goldenrod;
+                    break;
+                case 4:
+                    row4col1.BackColor = Color.Green;
+                    row4col2.BackColor = Color.Green;
+                    myWatch.Start();
+                    break;
+                default:
+                    break; 
+            }
+              
 
         }
 
+        #endregion
+
         private void goButton_Click(object sender, EventArgs e)
         {
-            // TODO - stop the stopwatch
+            myWatch.Stop();
 
-            // TODO - check if the ellapsed time in milliseconds is > 0. 
-            // If yes show the time.
-            // If no show "FOUL START" 
+            if (myWatch.ElapsedMilliseconds > 0)
+            {
+                TimeSpan elasped = myWatch.Elapsed;
+                timeLabel.Text = elasped.ToString(@"s\:fff");
+            }
+            else
+            {
+                timeLabel.Text = "FOUL START";
+            }
+           
 
         }
 
         private void resetButton_Click(object sender, EventArgs e)
         {
-            // TODO - reset the stopwatch
-
-            // TODO - put rows 1-3 colours back to DimGray and row 4 back to DarkOliveGreen
-
-            // TODO - reset row value and timeLabel text
+            myWatch.Reset();
+            row1col1.BackColor = Color.DimGray;
+            row1col2.BackColor = Color.DimGray;
+            row2col1.BackColor = Color.DimGray;
+            row2col2.BackColor = Color.DimGray;
+            row3col1.BackColor = Color.DimGray;
+            row3col2.BackColor = Color.DimGray;
+            row4col1.BackColor = Color.DarkOliveGreen;
+            row4col2.BackColor = Color.DarkOliveGreen;
+            currentRow = 0;
+            timeLabel.Text = "0.000";
+            lightTimer.Enabled = false;
 
         }
-
     }
 }
